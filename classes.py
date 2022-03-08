236e17dbb842837bade828a0aca5d957d645c936
Nazareno Garagiola <ngaragiola@jupiterace.famaf.unc.edu.ar>
2022-03-08 11:00:36 -0300

    first aproach to train and evaluate beto

---
+++ classes.py
@@ -0,0 +1,74 @@
+#classes.py
+
+class SentenceGetter(object): 
+    def __init__(self, dataset): 
+        self.n_sent = 1 
+        self.dataset = dataset 
+        self.empty = False 
+        agg_func = lambda s: [(w,t) for w,t in zip(s["word"].values.tolist(), 
+                                                   s["tag"].values.tolist())] 
+        self.grouped = self.dataset.groupby("sentence_id").apply(agg_func) 
+        self.sentences = [s for s in self.grouped] 
+     
+    def get_next(self): 
+        try: 
+            s = self.grouped["Sentence: {}".format(self.n_sent)] 
+            self.n_sent += 1 
+            return s 
+        except: 
+            return None 
+
+import torch
+from torch.utils.data import Dataset
+class CustomDataset(Dataset):
+    def __init__(self, tokenizer, sentences, labels, max_len):
+        self.len = len(sentences)
+        self.sentences = sentences
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        
+    def __getitem__(self, index):
+        sentence = str(self.sentences[index])
+        #esto del encode_plus medio que jode a veces, no se porque, puedo probar con usar Tokenizer de una
+        inputs = self.tokenizer.encode_plus(
+            sentence,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            pad_to_max_length=True,
+            #padding='max_length', #esto deberia de arreglar el warning de la linea de arriba
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        label = self.labels[index]
+        label.extend([4]*200)
+        label=label[:200]
+
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'tags': torch.tensor(label, dtype=torch.long)
+        } 
+    
+    def __len__(self):
+        return self.len
+
+
+from transformers import BertForTokenClassification, AutoModelForMaskedLM, AutoTokenizer, BertForTokenClassification, BertTokenizer, BertConfig, BertModel
+class BERTClass(torch.nn.Module):
+    def __init__(self, amount_tags):
+        super(BERTClass, self).__init__()
+        #for token classification or for masked LM? 
+        self.l1 = AutoModelForMaskedLM.from_pretrained("dccuchile/bert-base-spanish-wwm-cased", num_labels=amount_tags)
+        #self.l1 = BertForTokenClassification.from_pretrained('dccuchile/bert-base-spanish-wwm-cased', num_labels=amount_tags)
+        
+        # self.l2 = torch.nn.Dropout(0.3)
+        # self.l3 = torch.nn.Linear(768, 200)
+    
+    def forward(self, ids, mask, labels):
+        output_1= self.l1(ids, mask, labels = labels)
+        # output_2 = self.l2(output_1[0])
+        # output = self.l3(output_2)
+        return output_1
--- model.py
+++ model.py
@@ -1,5 +1,162 @@
-from transformers import AutoTokenizer, AutoModelForMaskedLM
+import torch
+import pandas as pd
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForMaskedLM, BertForTokenClassification, BertTokenizer, BertConfig, BertModel
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+from classes import CustomDataset, SentenceGetter, BERTClass
+
+#Pre Processing
+
+def parse_dataframe(df):
+    df['sentence_id'] = np.NaN
+    r = 0
+    for i, (n, _, _, _) in df.iterrows():
+        if n == 0:
+            df.iloc[i, df.columns.get_loc('sentence_id')] = f"sentence {r}"
+            r += 1
+
+  # pandas has a very handy "forward fill" function to fill missing values based on the last upper non-nan value
+    df = df.fillna(method='ffill')
+    return df
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print("device: ", device)
+
+df_train = pd.read_csv('data/conll/train.conllu', sep='\t', on_bad_lines='skip', engine='python')
+df_test = pd.read_csv('data/conll/test.conllu', sep='\t', on_bad_lines='skip', engine='python')
+
+labels_to_ids = {k: v for v, k in enumerate(df_train.tag.unique())}
+ids_to_labels = {v: k for v, k in enumerate(df_train.tag.unique())}
+
+train_getter = SentenceGetter(parse_dataframe(df_train))
+test_getter = SentenceGetter(parse_dataframe(df_test))
+
+tags_vals = list(set(df_train["tag"].values))
+tag2idx = {t: i for i, t in enumerate(tags_vals)}
+train_sentences = [' '.join([s[0] for s in sent]) for sent in train_getter.sentences]
+test_sentences = [' '.join([s[0] for s in sent]) for sent in test_getter.sentences]
+train_labels = [[s[1] for s in sent] for sent in train_getter.sentences]
+train_labels = [[tag2idx.get(l) for l in lab] for lab in train_labels]
+
+test_labels = [[s[1] for s in sent] for sent in test_getter.sentences]
+test_labels = [[tag2idx.get(l) for l in lab] for lab in test_labels]
+
+amount_tags = len(tags_vals) #len(df_train.tag.unique())
+
+# Configuration
 
 tokenizer = AutoTokenizer.from_pretrained("dccuchile/bert-base-spanish-wwm-cased")
+#model = AutoModelForMaskedLM.from_pretrained("dccuchile/bert-base-spanish-wwm-cased")
 
-model = AutoModelForMaskedLM.from_pretrained("dccuchile/bert-base-spanish-wwm-cased")
+MAX_LEN = 200
+TRAIN_BATCH_SIZE = 16 #aca tuve que bajarle un poco al batch size, sino se rompia
+VALID_BATCH_SIZE = 16
+EPOCHS = 5
+LEARNING_RATE = 2e-05
+
+train_sentences = train_sentences
+train_labels = train_labels 
+
+test_sentences = test_sentences
+test_labels = train_labels
+
+print(f"TRAIN Dataset: {len(train_sentences)} sentences")
+print(f"TEST Dataset: {len(test_sentences)} sentences")
+
+training_set = CustomDataset(tokenizer, train_sentences, train_labels, MAX_LEN)
+testing_set = CustomDataset(tokenizer, test_sentences, test_labels, MAX_LEN)
+
+train_params = {'batch_size': TRAIN_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+test_params = {'batch_size': VALID_BATCH_SIZE,
+                'shuffle': True,
+                'num_workers': 0
+                }
+
+training_loader = DataLoader(training_set, **train_params)
+testing_loader = DataLoader(testing_set, **test_params)
+
+model = BERTClass(amount_tags)
+model.to(device)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=LEARNING_RATE)
+
+def train(epoch):
+    model.train()
+    for _,data in enumerate(training_loader):
+        ids = data['ids'].to(device, dtype = torch.long)
+        mask = data['mask'].to(device, dtype = torch.long)
+        targets = data['tags'].to(device, dtype = torch.long)
+
+        loss = model(ids, mask, labels = targets)[0]
+
+        # optimizer.zero_grad()
+        if _%500==0:
+            print(f'Epoch: {epoch}, Loss:  {loss.item()}')
+        
+      #TO DO: probar con actualizar pesos luego
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+        #loss.backward()
+
+for e in range(EPOCHS):
+    train(e)
+
+#Evaluation
+
+from seqeval.metrics import f1_score
+
+def flat_accuracy(preds, labels):
+    flat_preds = np.argmax(preds, axis=2).flatten()
+    flat_labels = labels.flatten()
+    return np.sum(flat_preds == flat_labels)/len(flat_labels)
+
+def valid(model, testing_loader):
+    model.eval()
+    eval_loss = 0; eval_accuracy = 0
+    n_correct = 0; n_wrong = 0; total = 0
+    predictions , true_labels = [], []
+    nb_eval_steps, nb_eval_examples = 0, 0
+    with torch.no_grad():
+        for _, data in enumerate(testing_loader, 0):
+            ids = data['ids'].to(device, dtype = torch.long)
+            mask = data['mask'].to(device, dtype = torch.long)
+            targets = data['tags'].to(device, dtype = torch.long)
+
+            output = model(ids, mask, labels=targets)
+            loss, logits = output[:2]
+            logits = logits.detach().cpu().numpy()
+            label_ids = targets.to('cpu').numpy()
+            predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
+            true_labels.append(label_ids)
+            accuracy = flat_accuracy(logits, label_ids)
+            eval_loss += loss.mean().item()
+            eval_accuracy += accuracy
+            nb_eval_examples += ids.size(0)
+            nb_eval_steps += 1
+        eval_loss = eval_loss/nb_eval_steps
+        print(f"Validation loss: {format(eval_loss)}")
+        print(f"Validation Accuracy: {format(eval_accuracy/nb_eval_steps)}")
+        y_pred = []
+        for p in predictions:
+            s = []
+            for p_i in p: s.append(tags_vals[p_i])
+            y_pred.append(s)
+
+        y_true = []
+        for l in true_labels:
+          for l_i in l:
+            s = []
+            for l_ii in l_i: s.append(tags_vals[l_ii])
+            y_true.append(s)
+
+        #pred_tags = [tags_vals[p_i] for p in predictions for p_i in p]
+        #valid_tags = [tags_vals[l_ii] for l in true_labels for l_i in l for l_ii in l_i]
+        print(f"F1-Score: {f1_score(y_true, y_pred)}")
+
+valid(model, testing_loader)
