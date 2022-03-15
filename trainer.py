35ad84529bef6a547a19ed124d8e40db5627299a
Nazareno Garagiola <ngaragiola@jupiterace.famaf.unc.edu.ar>
2022-03-15 19:33:51 -0300

    Add a compute metrics that works, padding to be resolved

--- trainer.py
+++ trainer.py
@@ -21,7 +21,7 @@ print("device: ", device)
 
 MAX_LEN = 512  # el maximo de BERT
 AMOUNT_TAGS = 9
-PERC=7 #porcentaje del dataset que vamos a utilizar
+PERC = 5  # porcentaje del dataset que vamos a utilizar
 
 checkpoint = "dccuchile/bert-base-spanish-wwm-cased"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint, num_labels=AMOUNT_TAGS)
@@ -32,13 +32,18 @@ model = BertForTokenClassification.from_pretrained(
 
 from datasets import load_dataset, load_metric
 
+metric = load_metric("f1")
+
+
 def tokenize_function(example):
     return tokenizer(
         example["tokens"],
         is_split_into_words=True,
         truncation=True,
         max_length=MAX_LEN,
-        padding="max_length")
+        padding="max_length",
+    )
+
 
 def extend_labels(example):
     k = len(example["labels"])
@@ -51,31 +56,49 @@ def extend_labels(example):
 
 
 from transformers.trainer_utils import EvalPrediction
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support
-def compute_metrics(pred : EvalPrediction):
-    """Funcion que ejecuta el Trainer al evaluar"""
-    labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
-    precision, recall, f1, _ = precision_recall_fscore_support(labels,
-        preds,
-        average='binary')
-    acc = accuracy_score(labels, preds)
-    return {
-        'accuracy': acc,
-        'f1': f1,
-        'precision': precision,
-        'recall': recall
-    }
+from sklearn.metrics import (
+    accuracy_score,
+    precision_recall_fscore_support,
+    f1_score,
+)
 
+def compute_metrics(pred: EvalPrediction):
+  """Funcion que ejecuta el Trainer al evaluar"""
 
-train_ds, test_ds = load_dataset('conll2002', 'es', split=[f'train[:{PERC}%]', f'test[:{PERC}%]'])
-train_ds, test_ds = train_ds.rename_column('ner_tags', 'labels'), test_ds.rename_column('ner_tags', 'labels')
+  # Aca tengo que ver como usar la mascara para poder sacar todo lo que metio
+  # el padding
 
-train_ds = train_ds.map(tokenize_function, batched=True, remove_columns=['id', 'tokens', 'pos_tags'])
-test_ds = test_ds.map(tokenize_function, batched=True, remove_columns=['id', 'tokens', 'pos_tags'])
+  labels = pred.label_ids
+  preds = pred.predictions.argmax(-1)
 
-train_ds = train_ds.map(extend_labels) #, batched=True)
-test_ds = test_ds.map(extend_labels) #, batched=True)
+  #mini solucion, funciona pero hay que hacerlo mejor
+  flat_labels = [item for label_list in labels for item in label_list]
+  flat_preds = [item for label_list in preds for item in label_list]
+
+  f1 = f1_score(flat_labels, flat_preds, average="macro")
+  acc = accuracy_score(flat_labels, flat_preds)
+  return {"accuracy": acc,"f1": f1,}
+
+train_ds, test_ds = load_dataset(
+    "conll2002", "es", split=[f"train[:{PERC}%]", f"test[:{PERC}%]"]
+)
+train_ds, test_ds = train_ds.rename_column(
+    "ner_tags", "labels"
+), test_ds.rename_column("ner_tags", "labels")
+
+train_ds = train_ds.map(
+    tokenize_function,
+    batched=True,
+    remove_columns=["id", "tokens", "pos_tags"],
+)
+test_ds = test_ds.map(
+    tokenize_function,
+    batched=True,
+    remove_columns=["id", "tokens", "pos_tags"],
+)
+
+train_ds = train_ds.map(extend_labels)  # , batched=True)
+test_ds = test_ds.map(extend_labels)  # , batched=True)
 
 from transformers import DataCollatorWithPadding, TrainingArguments, Trainer
 
@@ -87,7 +110,7 @@ data_collator = DataCollatorWithPadding(
 # padding.
 
 training_args = TrainingArguments(
-    "betoNER-finetuned-CONLL",
+    f"betoNER-finetuned-CONLL-{PERC}",
     evaluation_strategy="epoch",
     per_device_train_batch_size=8,
     per_device_eval_batch_size=8,
@@ -101,18 +124,17 @@ training_args = TrainingArguments(
 trainer = Trainer(
     model,
     training_args,
-    ttrain_dataset=train_ds,
+    train_dataset=train_ds,
     eval_dataset=test_ds,
     optimizers=(torch.optim.AdamW(model.parameters()), None),
     data_collator=data_collator,
     tokenizer=tokenizer,
-    #compute_metrics=compute_metrics
+    compute_metrics=compute_metrics,
 )
 
 trainer.train()
 
-trainer.save_model("betoNER-finetuned-CONLL/trained_model/")
+trainer.save_model(f"betoNER-finetuned-CONLL-{PERC}/trained_model/")
 
-metrics=trainer.evaluate()
+metrics = trainer.evaluate()
 print(metrics)
-
