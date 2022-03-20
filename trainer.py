84bfe14e0d4ce1d4d7b612c588effdd3ab467807
Naza Gara <nazagara1277@gmail.com>
2022-03-20 18:15:11 -0300

    Trying to overfit model

--- trainer.py
+++ trainer.py
@@ -18,7 +18,7 @@ parser.add_argument(
     type=str,
     metavar="OUTPUT",
     help="Directory to store the model.",
-#    default="betoNER-finetuned-CONLL",
+    #    default="betoNER-finetuned-CONLL",
 )
 parser.add_argument(
     "--maxlength",
@@ -65,9 +65,8 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-OUTPUT_DIR = f'results/{args.output}'
+OUTPUT_DIR = f"results/{args.output}"
 MAX_LEN = args.maxlength
-AMOUNT_TAGS = 9
 PERC = args.percentage
 EPOCHS = args.epochs
 LEARNING_RATE = args.lr
@@ -81,43 +80,65 @@ TEST_BACTH_SIZE = args.test_batch_size
 # -> probar diferentes batch sizes, tambien por parametros
 # -> Parametros para guardar el modelo final ✓
 
+TAGS = [
+    "O",
+    "B-PER",
+    "I-PER",
+    "B-ORG",
+    "I-ORG",
+    "B-LOC",
+    "I-LOC",
+    "B-MISC",
+    "I-MISC",
+    "INWORD_TOKEN",
+]
+tag2id = {t: i for i, t in enumerate(TAGS)}
+id2tag = {i: t for i, t in enumerate(TAGS)}
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("device: ", device)
 
 checkpoint = "dccuchile/bert-base-spanish-wwm-cased"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, num_labels=AMOUNT_TAGS)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, num_labels=len(TAGS))
 model = BertForTokenClassification.from_pretrained(
-    checkpoint, num_labels=AMOUNT_TAGS
+    checkpoint, num_labels=len(TAGS)
 )
 # model = AutoModelForMaskedLM.from_pretrained(checkpoint, num_labels=AMOUNT_TAGS)
 
 from datasets import load_dataset, load_metric, Dataset
 
+
 def tokenize_and_align_labels(examples):
     """
-    Funcion para tokenizar las sentencias, ademas de alinear palabras con los 
+    Funcion para tokenizar las sentencias, ademas de alinear palabras con los
     labels. Usada junto al map de los Dataset, retorna otro Dataset, que
     ahora contiene labels, token_ids y attention_mask.
     """
-    tokenized_inputs = tokenizer(examples["tokens"],
-                                truncation=True,
-                                is_split_into_words=True,
-                                max_length=MAX_LEN,                                                     
-                                padding="max_length")
+    tokenized_inputs = tokenizer(
+        examples["tokens"],
+        truncation=True,
+        is_split_into_words=True,
+        max_length=MAX_LEN,
+        padding="max_length",
+    )
 
     labels = []
     for i, label in enumerate(examples[f"ner_tags"]):
-        word_ids = tokenized_inputs.word_ids(batch_index=i)  # Map tokens to their respective word.
+        word_ids = tokenized_inputs.word_ids(
+            batch_index=i
+        )  # Map tokens to their respective word.
         previous_word_idx = None
         label_ids = []
         for word_idx in word_ids:  # Set the special tokens to -100.
             if word_idx is None:
                 label_ids.append(-100)
-            elif word_idx != previous_word_idx:  # Only label the first token of a given word.
+            elif (
+                word_idx != previous_word_idx
+            ):  # Only label the first token of a given word.
                 label_ids.append(label[word_idx])
             else:
-                label_ids.append(tag2id['INWORD_TOKEN'])
+                label_ids.append(tag2id["INWORD_TOKEN"])
             previous_word_idx = word_idx
         labels.append(label_ids)
 
@@ -128,42 +149,38 @@ def tokenize_and_align_labels(examples):
 from transformers.trainer_utils import EvalPrediction
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 
+def flat_list(t):
+    return [item for sublist in t for item in sublist]
 
 def compute_metrics(pred: EvalPrediction):
     """
     Funcion que ejecuta el Trainer al evaluar, retorna un diccionario con la
-    precision y el f1-score. La 2da metrica es mejor cuando los datos tienen 
+    precision y el f1-score. La 2da metrica es mejor cuando los datos tienen
     mas desiguladad en las labels.
     """
-    labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
-    unpadding_labels, unpadding_preds = [], []
-    for i, s in enumerate(ds["tokens"]):
-        k = len(s)
-        unpadding_labels.append(labels[i][:k])
-        unpadding_preds.append(preds[i][:k])
-
-    flat_preds = [
-        item for label_list in unpadding_preds for item in label_list
-    ]
-    flat_labels = [
-        item for label_list in unpadding_labels for item in label_list
-    ]
-    print(
-        f"Length predictions:{len(flat_preds)}, Length labels: {len(flat_labels)}"
-    )
-
-    f1 = f1_score(flat_labels, flat_preds, average="micro")
-    acc = accuracy_score(flat_labels, flat_preds)
-    return {"accuracy": acc, "f1": f1}
+    metric = load_metric('f1')
+    logits, labels = pred
+    predictions = np.argmax(logits, axis=-1)
+    flat_labels, flat_preds = flat_list(labels), flat_list(predictions)
+    return metric.compute(predictions=flat_preds, references=flat_labels, average='micro')
 
 
 train_ds, test_ds, valid_ds = load_dataset(
-    "conll2002", "es", split=[f"train[:{PERC}%]", f"test[:{PERC}%]", f"validation[:{PERC}%]"]
+    "conll2002",
+    "es",
+    split=[f"train[:{PERC}%]", f"test[:{PERC}%]", f"validation[:{PERC}%]"],
 )
 
-train_ds = train_ds.map(tokenize_and_align_labels, batched=True, remove_columns=['id', 'pos_tags', 'ner_tags'])
-test_ds = test_ds.map(tokenize_and_align_labels, batched=True, remove_columns=['id', 'pos_tags', 'ner_tags'])
+train_ds = train_ds.map(
+    tokenize_and_align_labels,
+    batched=True,
+    remove_columns=["id", "pos_tags", "ner_tags"],
+)
+test_ds = test_ds.map(
+    tokenize_and_align_labels,
+    batched=True,
+    remove_columns=["id", "pos_tags", "ner_tags"],
+)
 
 from transformers import DataCollatorWithPadding, TrainingArguments, Trainer
 
@@ -192,7 +209,7 @@ trainer = Trainer(
     optimizers=(torch.optim.AdamW(model.parameters()), None),
     data_collator=data_collator,
     tokenizer=tokenizer,
-    #compute_metrics=compute_metrics,
+    compute_metrics=compute_metrics,
 )
 
 trainer.train()
@@ -209,7 +226,7 @@ from sklearn.metrics import accuracy_score, f1_score
 
 def evaluate(ds: Dataset):
     """
-    Para poder evaluar en base a un Dataset con el mismo formato que fue 
+    Para poder evaluar en base a un Dataset con el mismo formato que fue
     entrenado este modelo
     """
     predictions = trainer.predict(ds)
@@ -237,4 +254,4 @@ def evaluate(ds: Dataset):
 
 
 results = evaluate(train_ds)
-print(results)
+print(f"Results obtained: {results}")
