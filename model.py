cee638088c462ccee20ab5c13b3ef5082d76cf18
nazagara <nazagara1277@gmail.com>
2022-03-26 13:02:06 -0300

    train by sticking to original version

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ from transformers import (
 )
 from torch.utils.data import (
     Dataset,
+    TensorDataset,
     DataLoader,
     RandomSampler,
     SequentialSampler,
@@ -62,194 +63,176 @@ args = parser.parse_args()
 OUTPUT_DIR = f"results/{args.output}"
 MAX_LEN = args.maxlength
 BATCH_SIZE = args.batch_size
+EPOCHS = 5
+WEIGHT_DECAY = 0.0
+LEARNING_RATE = 2e-05
+
+from collections import namedtuple
+from torch.optim.lr_scheduler import LambdaLR
 
 
-# Pre Processing
-checkpoint = "dccuchile/bert-base-spanish-wwm-cased"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, num_labels=len(TAGS))
-model = AutoModelForTokenClassification.from_pretrained(
-    checkpoint, finetuning_task="conll2002", num_labels=len(TAGS)
+tokenizer = AutoTokenizer.from_pretrained(
+    "dccuchile/bert-base-spanish-wwm-cased", num_labels=len(LABEL_LIST)
 )
+model = BertForTokenClassification.from_pretrained(
+    "dccuchile/bert-base-spanish-wwm-cased", num_labels=len(LABEL_LIST)
+)
+Example = namedtuple("Example", ["tokens", "labels", "prediction_mask"])
 
 
 def read_conllu(file_name: str):
     f = open(f"{file_name}.conllu", "r", encoding="utf-8")
     token_lists = list(parse_incr(f, fields=["id", "word", "tag"]))
     f.close()
-    words, words_tags, labels = [], [], []
-    sen_idx, c = [], 0
-    for i, tl in enumerate(token_lists):
-        for j, t in enumerate(tl):
-            words.append(token_lists[i][j]["word"])
-            words_tags.append(token_lists[i][j]["tag"])
+    # words, words_tags = [], []
+    tokens, labels = [], []
+    for i, tl in tqdm(enumerate(token_lists), desc="parsing input"):
+        for j, _ in enumerate(tl):
 
-            tokenized = tokenizer.tokenize(token_lists[i][j]["word"])
-            labels += [
-                [LABEL_MAP[token_lists[i][j]["tag"]]]
-                + [LABEL_MAP[INWORD_PAD_LABEL]] * (len(tokenized) - 1)
-            ]
+            id = token_lists[i][j]
+            # words.append(id['word'])
+            # words_tags.append(id['tag'])
 
-            if token_lists[i][j]["id"] == 0:
-                sen_idx.append(f"sentence {c}")
-                c += 1
-            else:
-                sen_idx.append(np.NaN)
+            tokenized = tokenizer.tokenize(id["word"])
+            tokens += tokenized
+            labels += [id["tag"]] + [INWORD_PAD_LABEL] * (len(tokenized) - 1)
 
-    data = {
-        "sentence_id": sen_idx,
-        "word": words,
-        "tag": words_tags,
-        "labels": labels,
-    }
-    df = pd.DataFrame(data=data)
-    df = df.fillna(method="ffill")
-    return df
+    return tokens, labels
 
 
-def flat_list(t):
-    return [item for sublist in t for item in sublist]
+def generate_examples(tokens: list, labels: list):
+    half_len = MAX_LEN // 2
+    examples = []
+    for i in tqdm(range(0, len(tokens) - half_len, half_len), desc="Creating examples"):
+        token_window = tokens[i : i + 2 * half_len]
+        label_window = labels[i : i + 2 * half_len]
+
+        if i == 0:
+            prediction_mask = [1] * 2 * half_len
+        else:
+            prediction_mask = [0] * half_len + [1] * (len(token_window) - half_len)
+
+        assert len(token_window) == len(label_window) == len(prediction_mask)
+        example = Example(token_window, label_window, prediction_mask)
+        examples.append(example)
+    return examples
 
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("device: ", device)
+def create_dataset(examples: Example):
+    input_ids, labels, prediction_masks = [], [], []
+    for i, (tokens, ex_labels, prediction_mask) in enumerate(
+        tqdm(examples, desc="Converting examples")
+    ):
+        ids = tokenizer.convert_tokens_to_ids(tokens)
+        label_ids = [LABEL_MAP[l] for l in ex_labels]
 
-df_train = read_conllu("data/conll/train")
-df_test = read_conllu("data/conll/test")
+        # the last examples should be padded
+        if i == len(examples) - 1:
+            pad_length = MAX_LEN - len(tokens)
+            ids += [tokenizer.pad_token_id] * pad_length
+            label_ids += [IGNORE_INDEX] * pad_length
+            prediction_mask += [0] * pad_length
 
-train_getter = SentenceGetter(df_train)
-test_getter = SentenceGetter(df_test)
+        input_ids.append(ids)
+        labels.append(label_ids)
+        prediction_masks.append(prediction_mask)
 
-tags_vals = list(set(df_train["tag"].values))
-train_sentences = [" ".join([s[0] for s in sent]) for sent in train_getter.sentences]
-test_sentences = [" ".join([s[0] for s in sent]) for sent in test_getter.sentences]
-train_labels_from_df = [[s[1] for s in sent] for sent in train_getter.sentences]
-test_labels_from_df = [[s[1] for s in sent] for sent in test_getter.sentences]
-train_labels = list(map(flat_list, train_labels_from_df))
-test_labels = list(map(flat_list, test_labels_from_df))
+    # Verify that everything is the same length
+    for list_ in (input_ids, labels, prediction_masks):
+        for item in list_:
+            assert len(item) == MAX_LEN
 
-# Configuration
-
-# el original estaba en 200, pero uso el max de bert 512
-
-WEIGHT_DECAY = 0.1
-EPOCHS = 3
-LEARNING_RATE = 2e-05
-
-# test_labels = train_labels  # afirmo que usan las mismas labels
-
-print(f"TRAIN Dataset: {len(train_sentences)} sentences")
-print(f"TEST Dataset: {len(test_sentences)} sentences")
-
-training_set = CustomDataset(tokenizer, train_sentences, train_labels, MAX_LEN)
-testing_set = CustomDataset(tokenizer, test_sentences, test_labels, MAX_LEN)
-
-train_params = {
-    "batch_size": BATCH_SIZE,
-    "shuffle": True,
-    "num_workers": 0,
-}
-
-test_params = {
-    "batch_size": BATCH_SIZE,
-    "shuffle": True,
-    "num_workers": 0,
-}
-
-training_loader = DataLoader(training_set, **train_params)
-testing_loader = DataLoader(testing_set, **test_params)
-
-# model = BERTClass(len(TAGS))
-
-model.to(device)
-WARMUP_STEPS = 100
-total_steps = len(training_loader) * EPOCHS
-optimizer = torch.optim.AdamW(
-    model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
-)
-scheduler = get_linear_schedule_with_warmup(
-    optimizer,
-    num_warmup_steps=int(WARMUP_STEPS * total_steps),  # warmup is a %
-    num_training_steps=total_steps,
-)
+    dataset = TensorDataset(
+        torch.tensor(input_ids),
+        torch.tensor(labels),
+        torch.tensor(prediction_masks),
+    )
+    return dataset
 
 
-def train(epoch):
+from torch.optim import AdamW  # Aca cambie el Adam, por AdamW
+
+
+def pre_train(dataset: TensorDataset):
+    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+    total_steps = len(dataloader) * 3
+
+    no_decay = ["bias", "LayerNorm.weight"]
+    grouped_parameters = [
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": WEIGHT_DECAY,
+        },
+    ]
+    optimizer = AdamW(grouped_parameters, lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer,
+        num_warmup_steps=int(50 * total_steps),  # warmup is a %
+        num_training_steps=total_steps,
+    )
+    return dataloader, optimizer, scheduler
+
+
+def train(dataloader: DataLoader, optimizer: AdamW, scheduler: LambdaLR):
+    global_step, tr_loss, running_loss = 0, 0.0, 0.0
+    model.to(device)
     model.train()
-    for _, data in enumerate(training_loader):
-        ids = data["ids"].to(device, dtype=torch.long)
-        mask = data["mask"].to(device, dtype=torch.long)
-        targets = data["tags"].to(device, dtype=torch.long)
+    for _ in tqdm(range(EPOCHS), desc="Epoch"):
+        for step, batch in enumerate(dataloader):
+            batch = [t.to(device) for t in batch]
 
-        model.zero_grad()
-        optimizer.zero_grad()
+            model.zero_grad()
+            optimizer.zero_grad()
 
-        loss = model(ids, mask, labels=targets)[0]
+            # loss = model(batch[0], batch[2], labels = batch[1])[0]
+            loss = model(
+                input_ids=batch[0],
+                # notar que aca no le pasa la mask
+                labels=batch[1],
+            )[0]
+            loss.backward()
+            optimizer.step()
 
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
+            tr_loss += loss.item()
+            running_loss += loss.item()
+            global_step += 1
 
-        if _ % 500 == 0:
-            print(f"Epoch: {epoch}, Loss:  {loss.item()}")
+            scheduler.step()
+        # scheduler.step()
 
-
-for e in tqdm(range(EPOCHS), desc="Training Model"):
-    train(e)
-
-
-model.save_pretrained(OUTPUT_DIR)
-
-# Evaluation
-
-from seqeval.metrics import f1_score
+    print(global_step, tr_loss / global_step)
 
 
-def flat_accuracy(preds, labels):
-    flat_preds = np.argmax(preds, axis=2).flatten()
-    flat_labels = labels.flatten()
+def main():
+    train_tokens, train_labels = read_conllu("train")
+    test_tokens, test_labels = read_conllu("test")
 
-    with open(f"{OUTPUT_DIR}/preds", "+a") as f:
-        for item in flat_preds:
-            f.write("%s\n" % item)
+    train_examples = generate_examples(train_tokens, train_labels)
+    test_examples = generate_examples(test_tokens, test_labels)
 
-    with open(f"{OUTPUT_DIR}/labels", "+a") as f:
-        for item in flat_labels:
-            f.write("%s\n" % item)
+    frac = len(train_examples) // 10
+    train_examples = train_examples[:frac]
 
-    return np.sum(flat_preds == flat_labels) / len(flat_labels)
+    train_dataset = create_dataset(train_examples)
+    test_dataset = create_dataset(test_examples)
 
+    train_dl, optimizer, scheduler = pre_train(train_dataset)
 
-model.eval()
-eval_loss = 0
-eval_accuracy = 0
-n_correct = 0
-n_wrong = 0
-total = 0
-predictions, true_labels = [], []
-nb_eval_steps, nb_eval_examples = 0, 0
-with torch.no_grad():
-    for data in tqdm(training_loader, desc="Evaluation"):
-        ids = data["ids"].to(device, dtype=torch.long)
-        mask = data["mask"].to(device, dtype=torch.long)
-        targets = data["tags"].to(device, dtype=torch.long)
+    train(train_dl, optimizer, scheduler)
 
-        output = model(ids, mask, labels=targets)
-        loss, logits = output[:2]
-        logits = logits.detach().cpu().numpy()
-        label_ids = targets.to("cpu").numpy()
-        predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
-        true_labels.append(label_ids)
-        accuracy = flat_accuracy(logits, label_ids)
-        eval_loss += loss.mean().item()
-        eval_accuracy += accuracy
-        nb_eval_examples += ids.size(0)
-        nb_eval_steps += 1
-    eval_loss = eval_loss / nb_eval_steps
-    print(f"Validation loss: {format(eval_loss)}")
-    print(f"Validation Accuracy: {format(eval_accuracy/nb_eval_steps)}")
-
-
-def valid(model, testing_loader):
+    # evaluation
     model.eval()
     eval_loss = 0
     eval_accuracy = 0
@@ -259,40 +242,20 @@ def valid(model, testing_loader):
     predictions, true_labels = [], []
     nb_eval_steps, nb_eval_examples = 0, 0
     with torch.no_grad():
-        for _, data in enumerate(testing_loader, 0):
-            ids = data["ids"].to(device, dtype=torch.long)
-            mask = data["mask"].to(device, dtype=torch.long)
-            targets = data["tags"].to(device, dtype=torch.long)
+        for batch in tqdm(train_dl, desc="Evaluation"):
+            input_ids, labels, prediction_mask = [t.to(device) for t in batch]
 
-            output = model(ids, mask, labels=targets)
+            output = model(input_ids, prediction_mask, labels=labels)
             loss, logits = output[:2]
             logits = logits.detach().cpu().numpy()
-            label_ids = targets.to("cpu").numpy()
-            predictions.extend([list(p) for p in np.argmax(logits, axis=2)])
-            true_labels.append(label_ids)
-            accuracy = flat_accuracy(logits, label_ids)
-            eval_loss += loss.mean().item()
-            eval_accuracy += accuracy
-            nb_eval_examples += ids.size(0)
-            nb_eval_steps += 1
-        eval_loss = eval_loss / nb_eval_steps
-        print(f"Validation loss: {format(eval_loss)}")
-        print(f"Validation Accuracy: {format(eval_accuracy/nb_eval_steps)}")
-        y_pred = []
-        for p in predictions:
-            s = []
-            for p_i in p:
-                s.append(tags_vals[p_i])
-            y_pred.append(s)
+            label_ids = labels.to("cpu").numpy()
 
-        y_true = []
-        for l in true_labels:
-            for l_i in l:
-                s = []
-                for l_ii in l_i:
-                    s.append(tags_vals[l_ii])
-                y_true.append(s)
+    predictions = logits.argmax(-1)
+    print(predictions.shape)
+    print(predictions[0])
+    print(predictions[1])
+    print(predictions[2])
 
-        # pred_tags = [tags_vals[p_i] for p in predictions for p_i in p]
-        # valid_tags = [tags_vals[l_ii] for l in true_labels for l_i in l for l_ii in l_i]
-        print(f"F1-Score: {f1_score(y_true, y_pred)}")
+
+if __name__ == "__main__":
+    main()
