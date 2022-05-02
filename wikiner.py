a62f2cc33b37bebe50983ef2816da2d534da5a24
nazagara <nazagara1277@gmail.com>
2022-05-02 09:37:50 -0300

    Add script to load dataset to HF hub

---
+++ wikiner.py
@@ -0,0 +1,124 @@
+# coding=utf-8
+# Copyright 2020 HuggingFace Datasets Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+
+import datasets
+
+
+_CITATION = """/something"""
+
+_DESCRIPTION = """Dataset used to train a NER model"""
+
+_URL = "https://github.com/NazaGara/betoNER/tree/main/data/wikiner"
+_TRAINING_FILE = "train.conllu"
+
+
+class ConllppConfig(datasets.BuilderConfig):
+    """BuilderConfig"""
+
+    def __init__(self, **kwargs):
+        """BuilderConfig
+        Args:
+          **kwargs: keyword arguments forwarded to super.
+        """
+        super(ConllppConfig, self).__init__(**kwargs)
+
+
+class Conllpp(datasets.GeneratorBasedBuilder):
+    """Conllpp dataset."""
+
+    BUILDER_CONFIGS = [
+        ConllppConfig(
+            name="wikiner",
+            version=datasets.Version("1.0.0"),
+            description="wikiner dataset",
+        ),
+    ]
+
+    def _info(self):
+        return datasets.DatasetInfo(
+            description=_DESCRIPTION,
+            features=datasets.Features(
+                {
+                    "id": datasets.Value("string"),
+                    "tokens": datasets.Sequence(datasets.Value("string")),
+                    "ner_tags": datasets.Sequence(
+                        datasets.features.ClassLabel(
+                            names=[
+                                "O",
+                                "B-PER",
+                                "I-PER",
+                                "B-ORG",
+                                "I-ORG",
+                                "B-LOC",
+                                "I-LOC",
+                                "B-MISC",
+                                "I-MISC",
+                            ]
+                        )
+                    ),
+                }
+            ),
+            supervised_keys=None,
+            homepage=_URL,
+            citation=_CITATION,
+        )
+
+    def _split_generators(self, dl_manager):
+        """Returns SplitGenerators."""
+        urls_to_download = {
+            "train": f"{_URL}{_TRAINING_FILE}",
+        }
+        downloaded_files = dl_manager.download_and_extract(urls_to_download)
+
+        return [
+            datasets.SplitGenerator(
+                name=datasets.Split.TRAIN,
+                gen_kwargs={"filepath": downloaded_files["train"]},
+            ),
+        ]
+
+    def _generate_examples(self, filepath):
+        logging.info("⏳ Generating examples from = %s", filepath)
+        with open(filepath, encoding="utf-8") as f:  # CHECK ENCODING
+            guid = 0
+            tokens = []
+            ner_tags = []
+            for line in f:
+                if line.startswith("-DOCSTART-") or line == "" or line == "\n":
+                    if tokens:
+                        yield guid, {
+                            "id": str(guid),
+                            "tokens": tokens,
+                            "ner_tags": ner_tags,
+                        }
+                        guid += 1
+                        tokens = []
+                        ner_tags = []
+                else:
+                    # conll_like tokens are space separated
+                    splits = line.split(" ")
+                    tokens.append(splits[0])
+                    ner_tags.append(
+                        splits[1].rstrip()
+                    )  # ner_tags.append(splits[3].rstrip())
+            # last example
+            if tokens:
+                yield guid, {
+                    "id": str(guid),
+                    "tokens": tokens,
+                    "ner_tags": ner_tags,
+                }
